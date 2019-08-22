e8b331fcb0fcc3fab771760755659919e4b611e3
Eliran Wong <hkwebtech@gmail.com>
2019-08-22 16:04:31 +0100

    Add files via upload

---
+++ notes/exercise.dart
@@ -0,0 +1,65 @@
+// https://flutter.dev/docs/get-started/codelab
+
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Startup Name Generator',
+        home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+
+}
---
+++ notes/main.dart
@@ -0,0 +1,90 @@
+// Copyright 2019 Eliran Wong. All rights reserved.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(UniqueBibleApp());
+
+class UniqueBibleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Unique Bible App',
+        home: UniqueBibleApp(),
+    );
+  }
+}
+
+class UniqueBible extends StatefulWidget {
+  @override
+  UniqueBibleState createState() => UniqueBibleState();
+}
+
+class UniqueBibleState extends State<UniqueBible> {
+  final _fetchResults = [
+    {
+      "bNo": 0,
+      "cNo": 0,
+      "vNo": 0,
+      "vText": "King James Version"
+    },
+    {
+      "bNo": 1,
+      "cNo": 1,
+      "vNo": 1,
+      "vText": "In the beginning God created the heaven and the earth."
+    },
+    {
+      "bNo": 1,
+      "cNo": 1,
+      "vNo": 2,
+      "vText": "And the earth was without form, and void; and darkness [was] upon the face of the deep. And the Spirit of God moved upon the face of the waters."
+    },
+    {
+      "bNo": 1,
+      "cNo": 1,
+      "vNo": 3,
+      "vText": "And God said, Let there be light: and there was light."
+    },
+    {
+      "bNo": 1,
+      "cNo": 1,
+      "vNo": 4,
+      "vText": "And God saw the light, that [it was] good: and God divided the light from the darkness."
+    },
+    {
+      "bNo": 1,
+      "cNo": 1,
+      "vNo": 5,
+      "vText": "And God called the light Day, and the darkness he called Night. And the evening and the morning were the first day."
+    }
+  ];
+  final _bibleFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Unique Bible App'),
+      ),
+      body: _buildVerses(),
+    );
+  }
+
+  Widget _buildVerses() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          return _buildRow(_fetchResults[i]);
+        });
+  }
+
+  Widget _buildRow(String verseText) {
+    return ListTile(
+      title: Text(
+        verseText,
+        style: _bibleFont,
+      ),
+    );
+  }
+
+}
